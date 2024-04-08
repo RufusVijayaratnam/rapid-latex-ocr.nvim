6b575a30254ac7ea5423751b4c84f949b163a761
Rufus Vijayaratnam <rufusvijayaratnam@gmail.com>
2024-04-08 10:20:24 +0100

    minor change

--- lua/rapid-latex-ocr/init.lua
+++ lua/rapid-latex-ocr/init.lua
@@ -3,7 +3,7 @@ function M.setup()
     -- Register the "ImageToLatex" command directly here
     vim.api.nvim_create_user_command('ImageToLatex', function()
         -- Call the Python function exposed to Neovim
-        vim.call('ImageToLatex')
+      vim.fn['ImageToLatex']()
     end, {})
     -- Debug message to confirm the Lua part is loaded
     print("Rapid LaTeX OCR plugin loaded successfully.")
